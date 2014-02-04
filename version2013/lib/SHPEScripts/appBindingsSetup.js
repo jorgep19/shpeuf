fb1db6b81e878cefab21a3c51035767cbcb271b3
jorgep19 <jorgelpaez19@gmail.com>
2014-02-04 02:53:28 -0500

    Adding old version & basic setup for new version

---
+++ version2013/lib/SHPEScripts/appBindingsSetup.js
@@ -0,0 +1,48 @@
+ko.virtualElements.allowedBindings.updateListviewOnChange = true;
+ko.bindingHandlers.updateListviewOnChange = {
+  update: function (element, valueAccessor) {
+    ko.utils.unwrapObservable(valueAccessor());  //grab dependency
+
+    var listview = $(element).parents()
+                             .andSelf()
+                             .filter("[data-role='listview']");
+
+    if (listview) {
+      try {
+        $(listview).listview('refresh');
+      } catch (e) {
+        // if the listview is not initialised, the above call with throw an exception
+        // there doe snot appear to be any way to easily test for this state, so
+        // we just swallow the exception here.
+      }
+    }
+  }
+};
+
+//android blinking bug fix
+$(document).bind("mobileinit", function()
+{
+    if (navigator.userAgent.indexOf("Android") != -1)
+    {
+        $.mobile.defaultPageTransition = 'none';
+        $.mobile.defaultDialogTransition = 'none';
+    }
+});
+
+// create the various view models
+var companySearchVM = new CompanySearchVM(),
+companyDataVMAttached = new CompanyDataVM();
+companySearchVM.init(companiesData); 
+$.mobile.defaultPageTransition = "fade";
+
+$(document).ready(function () {
+  // bind each view model to a jQueryMobile page
+  ko.applyBindings(companySearchVM, document.getElementById("companies"));
+  ko.applyBindings(companyDataVMAttached, document.getElementById("companySummary"));
+  ko.applyBindings(companyDataVMAttached, document.getElementById("companyGlance"));
+  ko.applyBindings(companyDataVMAttached, document.getElementById("companyDetail"));
+});
+
+
+
+
